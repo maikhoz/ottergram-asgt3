c8f49abd559414a30e720f60f933333a83a1c6ea
Michael <weirdnamemike@gmail.com>
2017-02-21 02:07:25 -0800

    hijacks.js edit

--- ottergram/scripts/hijack.js
+++ ottergram/scripts/hijack.js
@@ -1,3 +1,9 @@
-document.addEventListener('click', function(event) {
-    event.preventDefault();
-});
+function linkHijack(document) {
+    'use strict';
+    document.addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+}
+
+var linkLists = [].slice.call(document.querySelectorAll('a'));
+linkLists.forEach(linkHijack);
